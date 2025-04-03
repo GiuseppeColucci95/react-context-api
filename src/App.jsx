b3b2a48d41065e7a802537648f2a83f5648d8622
GiuseppeColucci95 <colucci539@gmail.com>
2025-04-03 15:22:28 +0200

    Implement fetching and displaying posts in App component; add PostPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
-
+import { useEffect, useState } from "react"
 
 export default function App() {
 
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+
+    fetch('http://localhost:3000/posts')
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+
+        setPosts(data);
+      });
+  }, []);
+
+
   return (
     <>
+      <div className="container my-5">
 
+        <h1 className="text-center">React Context API</h1>
+
+        <div className="row mt-5">
+
+          {
+            posts.map(post => (
+              <div className="col">
+                <div className="card h-100">
+                  <h1>{post.title}</h1>
+                  <p>{post.content}</p>
+                </div>
+              </div>
+            ))
+          }
+        </div>
+      </div>
     </>
   )
 }
