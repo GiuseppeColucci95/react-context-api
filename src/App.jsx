554c92e209c7187f4ff5f6652ce5a9d5a37bdd95
GiuseppeColucci95 <colucci539@gmail.com>
2025-04-03 15:54:26 +0200

    Remove unnecessary blank lines in App and PostPage components

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,6 @@ export default function App() {
       });
   }, []);
 
-
   return (
     <>
       <PostsContext.Provider value={{ posts: posts }}>
--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -10,7 +10,6 @@ export default function PostPage() {
 
         <PostList></PostList>
       </div>
-
     </>
   );
 }
